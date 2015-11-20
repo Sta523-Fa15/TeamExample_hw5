724b6bd576037b13bb524c255c1ee42b2c5d8064
Colin Rundel <rundel@gmail.com>
2015-11-20 14:16:03 -0500

    In class update

--- sparkr.R
+++ sparkr.R
@@ -8,3 +8,42 @@ library(SparkR)
 sc = sparkR.init(master="yarn-client")
 sqlContext = sparkRSQL.init(sc)
 
+
+## json
+
+j = jsonFile(sqlContext, "hdfs://localhost:9000/data/short_1e6.json")
+
+res = count(group_by(j, "author"))
+res2 = collect(res)
+
+registerTempTable(j, "reddit")
+res = sql(sqlContext, "SELECT author, COUNT(*) as n FROM reddit GROUP BY author")
+res2 = collect(res)
+
+
+
+## df
+
+df = read.df(sqlContext, "hdfs://localhost:9000/data/short_1e6.json", source = "json")
+
+res = count(group_by(df, "author"))
+res2 = collect(res)
+
+registerTempTable(df, "reddit")
+res = sql(sqlContext, "SELECT author, COUNT(*) as n FROM reddit GROUP BY author")
+res2 = collect(res)
+
+sparkR.stop() # Stop sparkR
+
+
+
+## CSV
+
+Sys.setenv('SPARKR_SUBMIT_ARGS'='"--packages" "com.databricks:spark-csv_2.10:1.0.3" "sparkr-shell"')
+
+sc = sparkR.init(master="yarn-client")
+sqlContext = sparkRSQL.init(sc)
+
+house = read.df(sqlContext,"hdfs://localhost:9000/data/housing.txt", "com.databricks.spark.csv", header="false")
+
+sparkR.stop() # Stop sparkR
